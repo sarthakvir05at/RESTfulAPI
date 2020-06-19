6cf805cae7fb638dedc5945e8bf1d62ff25e4f06
Sarthak Jaiswal <sarthakjaiswal15@gmail.com>
2020-06-20 01:08:48 +0530

    Adding GET/ route so as to get/read from DB

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended:true }));
 app.use(express.static('public'));
 
-mongoose.connect("mongodb://localhost:27017/wikiDB", { useNewUrlParser: true });
+mongoose.connect("mongodb://localhost:27017/wikiDB", { useNewUrlParser: true, useUnifiedTopology: true });
 
 const articleSchema= new mongoose.Schema({
     title: {
@@ -22,6 +22,15 @@ const articleSchema= new mongoose.Schema({
 });
 
 const Article= mongoose.model("Article", articleSchema);
+
+app.get("/article", (req,res) => {
+    Article.find({}, (err,foundArticle) => {
+        if(!err)
+        res.send(foundArticle);
+        else
+        res.send(err);
+    })
+})
 
 app.listen(3000, () => {
     console.log('Server is up and ready');
