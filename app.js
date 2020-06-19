512a255bb51f7a3eba09ce107e727e24375496c0
Sarthak Jaiswal <sarthakjaiswal15@gmail.com>
2020-06-20 00:53:01 +0530

    Setting Up For RESTful API

---
+++ app.js
@@ -0,0 +1,28 @@
+const mongoose= require('mongoose');
+const bodyParser= require('body-parser');
+const ejs= require('ejs');
+const express= require('express');
+
+const app= express();
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended:true }));
+app.use(express.static('public'));
+
+mongoose.connect("mongodb://localhost:27017/wikiDB", { useNewUrlParser: true });
+
+const articleSchema= new mongoose.Schema({
+    title: {
+        required: true,
+        type: String
+    },
+    content: {
+        required: true,
+        type: String 
+    }
+});
+
+const Article= mongoose.model("Article", articleSchema);
+
+app.listen(3000, () => {
+    console.log('Server is up and ready');
+})
